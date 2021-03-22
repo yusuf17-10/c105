022e4f9194e5c8cad2e771c5dfd168d2ca8b4843
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2021-03-22 21:08:40 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,43 @@
+import csv 
+import math
+f=open("class2.csv",newline="")
+reader=csv.reader(f)
+
+data=list(reader)
+
+
+data.pop(0)
+total=0
+n=len(data)
+for new_data in data :
+    total+=float(new_data[1])
+
+mean=total/n
+
+print(mean)
+
+squared_list=[]
+
+for new_data in data :
+    a=float(new_data[1])-mean
+    a=a**2
+    squared_list.append(a)
+
+sum=0
+
+for j in squared_list :
+    sum+=j
+
+result=sum/(n-1)
+
+standard_deviation=math.sqrt(result)
+
+print(standard_deviation)
+
+
+import pandas as pd
+import plotly.express as px
+
+df=pd.read_csv("class2.csv")
+fig=px.scatter(df,x="Student Number",y="Marks")
+fig.show()
